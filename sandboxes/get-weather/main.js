50f50f76edf34c3cd0a9814e7c604e2ba99def83
Genbuchan <genbupower@gmail.com>
2019-11-05 15:45:58 +0900

    link fixed.

--- sandboxes/get-weather/main.js
+++ sandboxes/get-weather/main.js
@@ -10,8 +10,8 @@ let weather;
 let count = 0;
 
 let getWeather = function(loc, locward){
-  $.getJSON("http://weather.livedoor.com/forecast/webservice/json/v1?city=" + city, setWeatherData);
-  
+  $.getJSON("https://weather.livedoor.com/forecast/webservice/json/v1?city=" + city, setWeatherData);
+
   // city = unescape(encodeURIComponent(jsonData.))
   // ward = unescape(encodeURIComponent())
   console.log(JSON.stringify(jsonData))
